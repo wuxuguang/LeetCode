7c34d82fb6bf9770cf031010d4975c357652ffd9
wuxuguang <buptwxg@163.com>
2015-11-17 18:53:50 +0800

    Problem2 and Problem29 solution

---
+++ src/com/wxg/mysolution/list/ListNode.java
@@ -0,0 +1,10 @@
+package com.wxg.mysolution.list;
+
+/**
+ * Created by wuxuguang on 2015/11/17.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
